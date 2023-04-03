37ef28de93ee7125cd26bbc2387a25c3b1dcdd9d
xNissyx <126715665+xNissyx@users.noreply.github.com>
2023-04-03 12:41:48 +0000

    [finish] 3-7

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,7 @@
 Rails.application.routes.draw do
-  
-  get 'lists/new'
-  post "lists" => "lists#create"
-  
-  get 'lists/index'
-  
-  # as:オプションは名前付きルーティング、この場合URLの記述はlist_pathとなる
-  get "lists/:id" => "lists#show", as: "list"
-  
-  get "lists/:id/edit" => "lists#edit", as: "edit_list"
-  # updateのHTTPメソッドはpatchなことに注意
-  patch "lists/:id" => "lists#update", as: "update_list"
-  
+
+  resources :lists
+
   get '/top' => 'homes#top'
-  
+
 end
